37db85b859f8c70769861fa089d9fa6eab2b3102
Yechan <yechance7@gmail.com>
2025-07-24 01:17:32 +0900

    fix: enable userId parameter in batch infer

--- infer-server/src/main/config/Route.kt
+++ infer-server/src/main/config/Route.kt
@@ -4,26 +4,28 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ybigta.text2sql.infer.server.InferResp
 import io.ybigta.text2sql.infer.server.InferService
+import io.ybigta.text2sql.infer.core.InferResp 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.Json 
 
 @Serializable
-data class BatchRequest(
+internal data class InferRequest(
+    val userId: String,
+    val question: String
+)
+
+@Serializable
+internal data class BatchInferRequest(
     val userId: String,
     val questions: List<String>
 )
 
 @Serializable
-data class BatchResponse(val results: List<InferResp>)
-
-@Serializable
-data class InferRequest(
-    val userId: String,
-    val question: String
+internal data class BatchResponse(
+    val results: List<InferResp>
 )
 
 
@@ -34,14 +36,14 @@ internal fun Application.routeConfig(
     routing {
         post("/infer") {
             val inferRequest = call.receive<InferRequest>()
-            val inferResp = scope.async { inferService.infer(inferRequest.question, inferRequest.userId) }
+            val inferResp = scope.async { inferService.infer(inferRequest.question) }
             call.respond(inferResp.await())
         }
         post("/infer-batch") {
-            val batchRequest = call.receive<BatchRequest>()
+            val batchRequest = call.receive<BatchInferRequest>()
 
             val deferredResults = batchRequest.questions.map { question ->
-                scope.async { inferService.infer(question, batchRequest.userId) }
+                scope.async { inferService.infer(question) }
             }
 
             val results = deferredResults.map { it.await() }
